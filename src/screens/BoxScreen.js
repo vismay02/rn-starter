01b0a55dadcec3ae5cdfa2a9c326b7988531dab9
vismay02 <vismay.patil73@gmail.com>
2023-02-04 18:31:33 +0530

    Learned about layouts

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,39 +4,53 @@ import {StyleSheet, Text, TextInput, View } from "react-native";
 const BoxScreen = () => {
 
     return <View style={styles.viewStyle}>
-        <Text style={styles.textOneStyle}>Child #1</Text>
-        <Text style={styles.textTwoStyle}>Child #2</Text>
-        <Text style={styles.textThreeStyle}>Child #3</Text>
+        <View style={styles.viewOneStyle}/>
+        <View style={styles.viewTwoStyle}/>
+        <View style={styles.viewThreeStyle}/>
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
-        borderColor: 'black',
-        borderWidth: 3,
+        borderWidth:3,
+        height:100,
+        borderColor:'black',
         alignItems:"stretch",
-        flexDirection:"column",
-        height:200,
-        justifyContent:"space-evenly"
+        flexDirection:"row",
+        justifyContent:'space-between'
+    },
+    viewOneStyle: {
+        backgroundColor: 'red',
+        width:50,
+        height:50,
+    },
+    viewTwoStyle: {
+        backgroundColor: 'green',
+        width:50,
+        height:50,
+        alignSelf:'flex-end'    
+    },
+    viewThreeStyle: {
+        backgroundColor: 'blue',
+        width:50,
+        height:50
     },
     textOneStyle: {
         borderColor: 'red',
         borderWidth: 3,
-        flex:4,
-        alignSelf:"flex-start"
+        flex:1,
       },
       textTwoStyle: {
         borderColor: 'red',
         borderWidth: 3,
-        flex:4,
-        position:'absolute',
-        fontSize:28
+        flex:1,
+        fontSize:18,
+       ...StyleSheet.absoluteFillObject
           },
       textThreeStyle: {
         borderColor: 'red',
         borderWidth: 3,
-        flex:2,
-        alignSelf:"flex-end"
+        flex:1,
       }
 })
 
